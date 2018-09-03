f32f8fe03a3f00c4aa184df950ad50a6de0f4c50
Manish Kumar <37174461+Micky143@users.noreply.github.com>
2018-09-03 00:42:51 -0700

    Update PalindromeCheck.py

--- PalindromeCheck.py
+++ PalindromeCheck.py
@@ -6,6 +6,6 @@ print(word)
 reversed_word = word[::-1]
 
 if word == reversed_word:
-    print('you are enter the palindrme')
+    print('You are enter the palindrme')
 else:
-    print('you are did not enter the palindrme')
+    print('You are did not enter the palindrme')
